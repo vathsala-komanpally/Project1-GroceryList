626bfd739cac06beaebaf8acea1f9b8bf8f77c26
vathsala-komanpally <73576322+vathsala-komanpally@users.noreply.github.com>
2020-12-08 16:58:19 +0800

    choosen items in array of objects with their ID's

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 console.log("Javascript file is running");
 let nameOfItems=[];
+let itemID=0;
 $("button").on("click", (event) => {
+    event.preventDefault();
    const nameOfItem= event.target.innerHTML;
    console.log(nameOfItem);
+   const itemIdNumber=itemID++;
+   const idItemObject={id:itemIdNumber, itemName:nameOfItem};
+   nameOfItems.push(idItemObject);
+   console.log(nameOfItems);
 });
