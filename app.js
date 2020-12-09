6f40cc6e9139c5109324a5ea4a412aa7af059526
vathsala-komanpally <73576322+vathsala-komanpally@users.noreply.github.com>
2020-12-10 00:14:41 +0800

    displaying items choosen

--- app.js
+++ app.js
@@ -20,29 +20,50 @@ $("#finish").on("click", () => {
     numberOfItems.forEach((element, i) => {
         let itemPropertyName = element.Name;//other way to take name of element.["Name"]
         if (itemPropertyName in testObject) {
-            testObject[itemPropertyName].duplicate = true;
-            element.duplicate = true;
-            //     testObject.forEach((element, i) => { 
-
-            // });
+            //missing code to add price if item repeated
+            //testObject[itemPropertyName].duplicate = true;
+            //element.duplicate = true;
             let repeatValue = testObject[itemPropertyName].repeated;
-            console.log(repeatValue);
             repeatValue = repeatValue + 1;
             testObject[itemPropertyName].repeated = repeatValue;
             //testObject[itemPropertyName].repeated = element.repeated+1;
-            element.repeated = element.repeated + 1;
-            numberOfItems[i].repeated = element.repeated;
+            //element.repeated = repeatValue;
+            //numberOfItems[i].repeated = repeatValue;
             seenDuplicate = true;
-            delete element;
-            //numberOfItems.pop();
+            //delete element;
+
         }
         else {
             testObject[itemPropertyName] = element;
             delete element.duplicate;
-          
+
         }
     });
-
+    // numberOfItems.forEach((element, i) => {
+    //     for(i=1;i<numberOfItems.length;i++)
+    //     { //numberOfItems.indexOf(element.Name)
+    //         if(numberOfItems[i].Name==element.Name)
+    //         {
+    //             numberOfItems.splice(i,1);
+    //             i--;
+    //         }
+    //     }
+    // });
+    const objectLength=Object.keys(testObject).length;
+    numberOfItems.splice(objectLength,numberOfItems.length);
     console.log(testObject);
     console.log(numberOfItems);
+    printResult(numberOfItems);
 });
+
+function printResult(numberOfItems) {
+    // testObject.foreach((element, i)=>{
+    //      $("#result").append(`<li> ${element.Name} ${element.price}</li>`);
+
+    // });
+    numberOfItems.forEach((element, i) => {
+    
+        $("#result").append(`<li> ${element.Name} is: ${element.price}$ ${element.repeated} items selected</li>`);
+    })
+    //$("#result").append(`<h2>${i} ${element.Name} ${element.price}</h2>`);
+}
