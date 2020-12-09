c8cc4f58ae620dbf782ebb353ade509d4a902070
vathsala-komanpally <73576322+vathsala-komanpally@users.noreply.github.com>
2020-12-09 18:27:25 +0800

    removing repeted items and adding repeated value

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
 console.log("Javascript file is running");
-let nameOfItems=[];
-let itemID=0;
-$("button").on("click", (event) => {
+let numberOfItems = [];
+let itemID = 0;
+$(".btn").on("click", (event) => {
     event.preventDefault();
-   const nameOfItem= event.target.innerHTML;
-   console.log(nameOfItem);
-   const itemIdNumber=itemID++;
-   const idItemObject={id:itemIdNumber, itemName:nameOfItem};
-   nameOfItems.push(idItemObject);
-   console.log(nameOfItems);
+    const nameOfItem = event.target.innerHTML;
+    //$("#inputText").value(nameOfItem);
+    const itemIdNumber = event.target.id;
+    const itemPrice = Math.floor((Math.random() * 100) + 1);
+    //has to workout to print prive from html
+    const idItemObject = { id: itemIdNumber, Name: nameOfItem, price: itemPrice, repeated: 1 };
+    numberOfItems.push(idItemObject);
+    //checkingRepeatedItems();
+});
+
+$("#finish").on("click", () => {
+    console.log("finished click event"); let seenDuplicate = false;
+    testObject = {};
+    //checking item already choosen by user before
+    numberOfItems.forEach((element, i) => {
+        let itemPropertyName = element.Name;//other way to take name of element.["Name"]
+        if (itemPropertyName in testObject) {
+            testObject[itemPropertyName].duplicate = true;
+            element.duplicate = true;
+            //     testObject.forEach((element, i) => { 
+
+            // });
+            let repeatValue = testObject[itemPropertyName].repeated;
+            console.log(repeatValue);
+            repeatValue = repeatValue + 1;
+            testObject[itemPropertyName].repeated = repeatValue;
+            //testObject[itemPropertyName].repeated = element.repeated+1;
+            element.repeated = element.repeated + 1;
+            numberOfItems[i].repeated = element.repeated;
+            seenDuplicate = true;
+            delete element;
+            //numberOfItems.pop();
+        }
+        else {
+            testObject[itemPropertyName] = element;
+            delete element.duplicate;
+          
+        }
+    });
+
+    console.log(testObject);
+    console.log(numberOfItems);
 });
